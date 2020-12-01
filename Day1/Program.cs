44705e008cc1262fa4c6ab7b51de3b477bfe443c
Piotr Kazimierski <piotr.kazimierski92@wp.pl>
2020-12-01 08:00:40 +0100

    Part2 done, with algorithm found on geeksforgeeks.org tho

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -6,6 +6,24 @@ namespace Day1
 {
     internal class Program
     {
+        static void findTriplets(int[] arr, int n, int sum)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                HashSet<int> s = new HashSet<int>();
+                for (int j = i + 1; j < n; j++)
+                {
+                    int x = sum - (arr[i] + arr[j]);
+                    if (s.Contains(x))
+                    {
+                        Console.Write("{0} {1} {2}\n", x, arr[i], arr[j]);
+                        Console.WriteLine(x * arr[i] * arr[j]);
+                    } 
+                    else
+                        s.Add(arr[j]);
+                }
+            }
+        }
         private static void Main(string[] args)
         {
             var inputFile = File.ReadAllLines("input.txt");
@@ -20,16 +38,32 @@ namespace Day1
                 numberListAsInts.Add(Int32.Parse(number));
             }
 
+            #region Part1
             for (int i = 0; i < numberListAsInts.Count - 1; i++)
                 for (int j = i + 1; j < numberListAsInts.Count; j++)
                 {
                     var tempSumResult = numberListAsInts[i] + numberListAsInts[j];
                     if (tempSumResult == searchedValue)
                     {
-                        Console.WriteLine("Result numbers = " + numberListAsInts[i] +" "+ numberListAsInts[j]);
+                        Console.WriteLine("Result numbers = " + numberListAsInts[i] + " " + numberListAsInts[j]);
                         Console.WriteLine("Multiplication of result numbers = " + numberListAsInts[i] * numberListAsInts[j]);
                     }
                 }
+            #endregion
+
+            #region Part2
+            findTriplets(numberListAsInts.ToArray(), numberListAsInts.ToArray().Length, searchedValue);
+            #endregion
+
+
+
+
+
         }
+
+
+
+
     }
 }
+    
