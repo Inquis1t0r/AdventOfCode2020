d75d0e521c03dbda07b12ac9168f38638b9b86f9
Piotr Kazimierski <piotr.kazimierski92@wp.pl>
2020-12-01 07:41:26 +0100

    input read from file to int list

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var inputFile = File.ReadAllLines("input.txt");
+            var numberList = new List<string>(inputFile);
+            var numberListAsInts = new List<int>();
+
+            int searchedValue = 2020;
+
+            foreach (var number in numberList)
+            {
+                //System.Console.WriteLine(number);
+                numberListAsInts.Add(Int32.Parse(number));
+            }
+
+            for (int i = 0; i < numberListAsInts.Count - 1; i++)
+                for (int j = i + 1; j < numberListAsInts.Count; j++)
+                {
+                    Console.WriteLine("{0} against {1}", numberListAsInts[i], numberListAsInts[j]);
+                }
+
+
         }
     }
 }
