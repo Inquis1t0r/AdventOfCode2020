4470771ad1bef2b8aa08e1c9f6a63f9d8640c8e0
Piotr Kazimierski <piotr.kazimierski92@wp.pl>
2020-12-02 11:37:43 +0100

    Day2 added

---
+++ Day2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = File.ReadAllLines("input.txt");
+            var passwordsWithRulesList = new List<string>(inputFile);
+            int validPasswordsCound = 0;
+
+            foreach (var line in passwordsWithRulesList)
+            {
+                //System.Console.WriteLine(line);
+
+                if (true)
+                {
+                    validPasswordsCound++;
+                }
+            }
+
+            Console.WriteLine(validPasswordsCound);
+
+        }
+    }
+}
