faf1f9f4a63df6f511b1d573c401c659fcd22e4e
Piotr Kazimierski <piotr.kazimierski92@wp.pl>
2020-12-02 12:15:07 +0100

    Day2 Part 2 Basic setup

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -13,8 +13,9 @@ namespace Day2
             var passwordsWithRulesList = new List<string>(inputFile);
             int validPasswordsCount = 0;
 
+            Console.WriteLine("Part 1:");
             foreach (var line in passwordsWithRulesList)
-            {
+            {          
 
                 string[] parts = line.Split(' ');
                 //Console.WriteLine("[{0}]", string.Join(", ", parts));
@@ -30,32 +31,87 @@ namespace Day2
                 int maxNumberOfLetters = Int32.Parse(MinMaxValues[1]);
 
 
-                Console.WriteLine("Min num " + minNumberOfLetters + " max num "  + maxNumberOfLetters);
+               // Console.WriteLine("Min num " + minNumberOfLetters + " max num "  + maxNumberOfLetters);
 
                 char letterToCheck = char.Parse(parts[1].Substring(0,1));
                 String checkedPassword = parts[2];
-                Console.WriteLine("Password to check: " + checkedPassword);
-                Console.WriteLine("Char to check: " + letterToCheck);
+               // Console.WriteLine("Password to check: " + checkedPassword);
+               // Console.WriteLine("Char to check: " + letterToCheck);
 
                 //int frequencyOfChar = line.Where(x => (x.Equals(letterToCheck))).Count();
                 int frequencyOfChar = line.Split(letterToCheck).Length - 2;  //This accutalu counts resulting subststrings
-                Console.WriteLine("Occurences of char: " + frequencyOfChar);
+              //  Console.WriteLine("Occurences of char: " + frequencyOfChar);
 
                 if (frequencyOfChar >= minNumberOfLetters && frequencyOfChar <= maxNumberOfLetters)
                 {
                     validPasswordsCount++;
-                    Console.WriteLine("Valid password");
+                  //  Console.WriteLine("Valid password");
                 }
                 else
                 {
-                    Console.WriteLine("Invalid password");
+                   // Console.WriteLine("Invalid password");
                 }
+
+               // Console.WriteLine();
+
+            }
+
+            //Console.WriteLine("Number of valid password: " + validPasswordsCount);
+
+
+
+            Console.WriteLine("Part 2:");
+            foreach (var line in passwordsWithRulesList)
+            {
+
+                string[] parts = line.Split(' ');
+                //Console.WriteLine("[{0}]", string.Join(", ", parts));
+
+                string rules = parts[0];
+
+                String[] MinMaxValues = rules.Split('-');
+
+                //Console.WriteLine("[{0}]", string.Join(", ", MinMaxValues));
+
+
+                int pos1 = Int32.Parse(MinMaxValues[0]);
+                int pos2 = Int32.Parse(MinMaxValues[1]);
+
+
+               // Console.WriteLine("Min num " + minNumberOfLetters + " max num " + maxNumberOfLetters);
+
+                char letterToCheck = char.Parse(parts[1].Substring(0, 1));
+                String checkedPassword = parts[2];
+               // Console.WriteLine("Password to check: " + checkedPassword);
+              //  Console.WriteLine("Char to check: " + letterToCheck);
+
+                //int frequencyOfChar = line.Where(x => (x.Equals(letterToCheck))).Count();
+                int frequencyOfChar = line.Split(letterToCheck).Length - 2;  //This accutalu counts resulting subststrings
+                Console.WriteLine("Occurences of char: " + frequencyOfChar);
+                int temp = 0;
+                if (checkedPassword[pos1-1].Equals(letterToCheck))
+                {
+                    temp++;
+                   // Console.WriteLine("Valid password");
+                }
+                if (checkedPassword[pos2-1].Equals(letterToCheck))
+                {
+                    temp++;
+                    //Console.WriteLine("Valid password");
+                }
+
+                if(temp == 1)
+                {
+                    validPasswordsCount++;
+                }
+
 
                 Console.WriteLine();
 
             }
 
             Console.WriteLine("Number of valid password: " + validPasswordsCount);
+
         }
     }
 }
